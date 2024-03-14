c9cd335c38586fad3f69c7b4803f9a150951d9cf
Brayanjax <120285750+Brayanjax@users.noreply.github.com>
2024-03-14 01:07:21 -0600

    Funcionalidad del Inicio de sesion
    Se implemento la funcionalidad del usuario poder logearse con su gmail y contraseña en el dado caso que ambas sean correctas podra entrar a la pagina del calendario, falta agregar el calendario

--- app.ts
+++ app.ts
@@ -1,86 +1,113 @@
-var API_URL_BASE = 'https://task-manager-backend2.azurewebsites.net';
+
+
+var API_URL_BASE  = 'https://task-manager-backend2.azurewebsites.net';
+  
+
 //movimiento
-var registryBtn = document.getElementById('registroBtn');
-registryBtn.addEventListener('click', function () {
-    document.getElementById('ladoIzquierdo').style.display = 'none';
-    document.getElementById('imagenIzquierda').style.display = 'none';
-    document.getElementById('ladoDerecho').style.display = 'block';
-    document.getElementById('imagenDerecha').style.display = 'block';
+const registryBtn = document.getElementById('registroBtn') as HTMLInputElement;
+ registryBtn.addEventListener('click', function() {  
+    document.getElementById('ladoIzquierdo')!.style.display = 'none';
+    document.getElementById('imagenIzquierda')!.style.display = 'none';
+    document.getElementById('ladoDerecho')!.style.display = 'block';  
+    document.getElementById('imagenDerecha')!.style.display = 'block'; 
+}); 
+const loginBtn = document.getElementById('inicioBtn') as HTMLInputElement;
+ loginBtn.addEventListener('click', function() {
+    document.getElementById('ladoDerecho')!.style.display = 'none';
+    document.getElementById('imagenDerecha')!.style.display = 'none';
+    document.getElementById('ladoIzquierdo')!.style.display = 'block';
+    document.getElementById('imagenIzquierda')!.style.display = 'block';
 });
-var loginBtn = document.getElementById('inicioBtn');
-loginBtn.addEventListener('click', function () {
-    document.getElementById('ladoDerecho').style.display = 'none';
-    document.getElementById('imagenDerecha').style.display = 'none';
-    document.getElementById('ladoIzquierdo').style.display = 'block';
-    document.getElementById('imagenIzquierda').style.display = 'block';
-});
-function CreatePerson(nameId, lastNameId, emailId, passwordId) {
-    var nameInput = document.getElementById(nameId);
-    var lastNameInput = document.getElementById(lastNameId);
-    var emailInput = document.getElementById(emailId);
-    var passwordInput = document.getElementById(passwordId);
-    var name = nameInput.value;
-    var lastName = lastNameInput.value;
-    var email = emailInput.value;
-    var password = passwordInput.value;
-    var user = {
-        id: email,
+
+
+
+
+function CreatePerson(nameId: string, lastNameId: string, emailId: string, passwordId: string) {
+
+    let nameInput: HTMLInputElement = document.getElementById(nameId) as HTMLInputElement;
+    let lastNameInput: HTMLInputElement = document.getElementById(lastNameId) as HTMLInputElement;
+    let emailInput: HTMLInputElement = document.getElementById(emailId) as HTMLInputElement;
+    let passwordInput: HTMLInputElement = document.getElementById(passwordId) as HTMLInputElement;
+
+let name: string = nameInput.value;
+let lastName: string = lastNameInput.value;
+let email: string = emailInput.value;
+let password: string = passwordInput.value;
+  
+    const user : {
+        id: string, 
+        name: string,
+        lastname: string,
+        email: string,
+        password: string
+    }={
+        id: email, 
         name: name,
         lastname: lastName,
         email: email,
         password: password
     };
-    var apiUrl = API_URL_BASE + '/api/Users/CreateUser';
-    $.ajax({
+
+    var apiUrl  = API_URL_BASE + '/api/Users/CreateUser';
+ 
+    $.ajax({   
         url: apiUrl,
         method: "POST",
         contentType: "application/json;charset=utf-8",
         dataType: "json",
         data: JSON.stringify(user)
     }).done(function (outcome) {
+        
         Swal.fire({
             icon: 'success',
             title: 'Completado',
             text: 'Respuesta correcta: ' + JSON.stringify(outcome),
             confirmButtonColor: 'var(--dark-purple)',
             confirmButtonText: 'Aceptar'
-        });
+        })
     }).fail(function (xhr, status, error) {
         alert("Ha ocurrido un error: " + error + "\nEstado: " + status + "\nDetalles: " + xhr.responseText);
     });
 }
+
+
+    
 //hacer funcion de comprovacion
-function GetUser(emailI, passwordId) {
-    var emailInput = document.getElementById(emailI);
-    var passwordInput = document.getElementById(passwordId);
-    var email = emailInput.value;
-    var password = passwordInput.value;
+function GetUser(emailI: string, passwordId: string) {
+    let emailInput: HTMLInputElement = document.getElementById(emailI) as HTMLInputElement;
+    let passwordInput: HTMLInputElement = document.getElementById(passwordId) as HTMLInputElement;
+    var email: string = emailInput.value;
+    var password: string = passwordInput.value;
+
     var apiUrl = API_URL_BASE + '/api/Users/GetUser/' + encodeURIComponent(email);
-    console.log("hola");
+    console.log("hola")
     $.ajax({
         url: apiUrl,
         method: "GET",
         contentType: "application/json;charset=utf-8",
         dataType: "json"
     }).done(function (outcome) {
+       
         if (outcome && outcome.email && outcome.password) {
-            var responseEmail = outcome.email;
+            var responseEmail : string = outcome.email;
             var responsePassword = outcome.password;
+
+            
             console.log('Email recibido:', responseEmail);
             console.log('Contraseña recibida:', responsePassword);
-            if (responseEmail === email && responsePassword === password) {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Completado',
-                    text: 'Respuesta correcta: ' + JSON.stringify(outcome),
-                    confirmButtonColor: 'var(--dark-purple)',
-                    confirmButtonText: 'Aceptar'
-                }).then(function () {
-                    // Redirigir a la página deseada después de que el usuario haya hecho clic en Aceptar
-                    window.location.href = 'pagina.html';
-                });
-            }
-            else {
+            if(responseEmail===email && responsePassword===password){
+                
+            Swal.fire({
+                icon: 'success',
+                title: 'Completado',
+                text: 'Respuesta correcta: ' + JSON.stringify(outcome),
+                confirmButtonColor: 'var(--dark-purple)',
+                confirmButtonText: 'Aceptar'
+            }).then(() => {
+                // Redirigir a la página deseada después de que el usuario haya hecho clic en Aceptar
+                window.location.href = 'pagina.html';
+            });
+            }else{
                 Swal.fire({
                     icon: 'error',
                     title: 'Completado',
@@ -89,14 +116,18 @@ function GetUser(emailI, passwordId) {
                     confirmButtonText: 'Aceptar'
                 });
             }
-        }
-        else {
+            
+           
+            
+        } else {
+            
             alert('La respuesta de la API no contiene los campos esperados.');
         }
     }).fail(function (xhr, status, error) {
         alert("Ha ocurrido un error: " + error + "\nEstado: " + status + "\nDetalles: " + xhr.responseText);
     });
 }
+
 /*
 function CreateUser() {
     this.SubmitPersonToActivate = function (nombre,apellido,gmail,contraseña) {
@@ -143,3 +174,5 @@ function CreateUser() {
     });
     }
 }*/
+    
+       
